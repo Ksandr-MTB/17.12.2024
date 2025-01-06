3627b0d46fff3b9b36c31ab198325d16f87c88c7
user <sasha.sapegin.99@gmail.com>
2025-01-06 07:20:27 +0700

    12

---
+++ module_0_1.py
@@ -0,0 +1,19 @@
+# "1st program"
+print(9**0.5*5)
+
+# "2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+# "4th program"
+dana_stroka='123.456'
+n=float(dana_stroka)
+n=n*10
+n=n%10
+n=n//1
+n=int(n)
+print(n)
